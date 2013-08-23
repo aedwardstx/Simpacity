65639e50405e07ce97e39a0d9534a49caf313e08
Andrew Edwards <Andrew.Edwards@Rackspace.com>
2013-08-23 15:52:27 +0000

    Fixed button sizing

--- lib/SimpacityAutoconfig.rb
+++ lib/SimpacityAutoconfig.rb
@@ -29,4 +29,16 @@ require "#{simpacity_base}/lib/SimpacityExtensionCommon.rb"
 #also affects percentile calculation, think hard before you change this to something other than 100.
 sliceSize = Setting.first.slice_size
 
+Device.each do |device|
+  #get all ints from interfaces
+  #each int from mongo, is already added? no?, does it match the expressions? yes? add it
 
+  interfaces = get_interfaces_for_device(id)
+
+  interfaces.each do |int|
+    (bandwidth, description) = get_interface_metadata_by_name(device.hostname,int)
+    dev_find = device.interface.find(:name => int).length
+    
+
+  end
+end 
--- lib/SimpacityExtensionCommon.rb
+++ lib/SimpacityExtensionCommon.rb
@@ -12,8 +12,16 @@ simpacity_base = '/opt/simpacity-dev'
 require "#{simpacity_base}/lib/SimpacityMath"
 
 ActiveRecord::Base.establish_connection(
-    :adapter => 'sqlite3',
-    :database => "#{simpacity_base}/db/development.sqlite3"
+    #:adapter => 'sqlite3',
+    #:database => "#{simpacity_base}/db/development.sqlite3"
+    :adapter => 'mysql',
+    :encoding => 'utf8',
+    :database => 'simpacity_dev',
+    :username => 'simpacity_dev',
+    :password => 'simpacity_dev_secret',
+    :socket => '/var/run/mysqld/mysqld.sock',
+    :pool => 5,
+    :timeout => 5000
 )
 
 #Require the Simpacity model files
